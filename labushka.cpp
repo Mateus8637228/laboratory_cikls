2921e8ddfb440a04805fa76f0093ff16af440f77
Mateus8637228 <matrac2k20@gmail.com>
2025-09-29 00:48:07 +0700

    labushka.cpp
    переделал все добро, надеюсь хоть сейчас получше)

--- labushka.cpp
+++ labushka.cpp
@@ -13,26 +13,38 @@ int main() {
   double C = 0.81e-7;
   const double PI = 3.141592653589793;
 
-  double Lvals[] = { 0.05, 0.06, 0.07, 0.08, 0.09, 0.10, 0.20, 0.30, 0.40, 0.50 };
-  int n = sizeof(Lvals) / sizeof(Lvals[0]);
-
   cout << fixed << setprecision(3);
   cout << "   L        T" << endl;
 
-  for (int i = 0; i < n; i++) {
-    double L = Lvals[i];
-
+  double L = 0.05;
+  while (L <= 0.10 + 1e-9) {
     double term = 1.0 / (L * C) - pow(R / (2.0 * L), 2);
 
     if (term <= 0.0) {
       cout << setw(8) << L << "    " << "---" << endl;
-    }
-    else {
+    } else {
       double omega = sqrt(term);
       double T = 2000.0 * PI / omega;
       cout << setw(8) << L << "    " << T << endl;
     }
+        
+    L += 0.01;
   }
+
+  L = 0.20;
+  do {
+    double term = 1.0 / (L * C) - pow(R / (2.0 * L), 2);
+
+    if (term <= 0.0) {
+      cout << setw(8) << L << "    " << "---" << endl;
+    } else {
+      double omega = sqrt(term);
+      double T = 2000.0 * PI / omega;
+      cout << setw(8) << L << "    " << T << endl;
+    }
+        
+    L += 0.10;
+  } while (L <= 0.50 + 1e-9);
 
   return 0;
 }
