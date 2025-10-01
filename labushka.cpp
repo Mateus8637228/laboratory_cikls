a015aedd6f67b79000203b66158e97fcc5c1186e
Mateus8637228 <matrac2k20@gmail.com>
2025-10-02 00:11:39 +0700

    Update labushka.cpp

--- labushka.cpp
+++ labushka.cpp
@@ -9,42 +9,41 @@
 using namespace std;
 
 int main() {
-  double R = 1530.0;
-  double C = 0.81e-7;
+  double R, C, L;
   const double PI = 3.141592653589793;
-
+  double term, omega, T;
+    
+  R = 1530.0;
+  C = 0.000000081;
+    
   cout << fixed << setprecision(3);
   cout << "   L        T" << endl;
 
-  double L = 0.05;
-  while (L <= 0.10 + 1e-9) {
-    double term = 1.0 / (L * C) - pow(R / (2.0 * L), 2);
-
-    if (term <= 0.0) {
+  L = 0.05;
+  while (L <= 0.10) {
+    term = 1.0 / (L * C) - pow(R / (2.0 * L), 2);
+    if (term < 0) {
       cout << setw(8) << L << "    " << "---" << endl;
     } else {
-      double omega = sqrt(term);
-      double T = 2000.0 * PI / omega;
+      omega = sqrt(term);
+      T = 2000.0 * PI / omega;
       cout << setw(8) << L << "    " << T << endl;
     }
-        
     L += 0.01;
   }
 
   L = 0.20;
   do {
-    double term = 1.0 / (L * C) - pow(R / (2.0 * L), 2);
-
-    if (term <= 0.0) {
+    term = 1.0 / (L * C) - pow(R / (2.0 * L), 2);
+    if (term < 0) {
       cout << setw(8) << L << "    " << "---" << endl;
     } else {
-      double omega = sqrt(term);
-      double T = 2000.0 * PI / omega;
+      omega = sqrt(term);
+      T = 2000.0 * PI / omega;
       cout << setw(8) << L << "    " << T << endl;
     }
-        
     L += 0.10;
-  } while (L <= 0.50 + 1e-9);
+  } while (L <= 0.50);
 
   return 0;
 }
